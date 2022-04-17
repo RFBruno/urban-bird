7864a39e49e2e1a99bc124944d752c987af12205
RFBruno <bruno_rafael_08@hotmail.com>
2022-04-17 20:54:10 -0300

    Oferta filter modified for subject type

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -9,20 +11,26 @@ import { Oferta } from '../shared/oferta.model';
 })
 export class TopoComponent implements OnInit {
 
-  public ofertas: Oferta[]  = [];
+  public ofertas?: Observable<Oferta[]>;
+  private subjectOfertas: Subject<string> = new Subject<string>();
 
   constructor(
     private ofertasServices: OfertasService
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.ofertas = this.subjectOfertas
+      .pipe(
+        switchMap((termo:string) =>{
+          return this.ofertasServices.pesquisaOfertas(termo);
+        })
+      );
+
+    this.ofertas.subscribe((ofertas:Oferta[]) => console.log(ofertas));
   }
 
   public pesquisar(termo: string): void{
-    let observableOfertas = this.ofertasServices.pesquisaOfertas(termo)
-    observableOfertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas)
-    );
+   this.subjectOfertas.next(termo);
   }
 
 }
